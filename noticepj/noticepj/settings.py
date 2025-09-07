e88d373f9cfdc993f7d211a6d94a620e904b9467
guyue <2583526329@qq.com>
2025-09-07 20:50:15 +0800

    create new pj

--- noticepj/noticepj/settings.py
+++ noticepj/noticepj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a3ik@f_=embpezwv!lsfim2q$3wkixv4mx=w9v3$3qok+ofggy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",]
 
 
 # Application definition
